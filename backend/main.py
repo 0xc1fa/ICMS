47d0c2ac237e3de4e97d095b2cae309b6b5e066c
charlckh <124652158+charlckh@users.noreply.github.com>
2023-11-14 12:09:36 +0800

    Update main.py
    Updated LoginHistory Insert, Put and Update

--- backend/main.py
+++ backend/main.py
@@ -59,6 +59,30 @@ def get_student_by_id(id):
     cursor.close()
     return {"status": "ok", "rows": rows}
 
+@app.get("/login-history/get/{id}")
+def get_login_history_by_id(id):
+    cursor = cnx.cursor()
+    cursor.execute(f"SELECT * FROM LoginHistory WHERE student_id = {id};")
+    rows = [dict(zip(cursor.column_names, row)) for row in cursor]
+    cursor.close()
+    return {"status": "ok", "rows": rows}
 
+@app.get("/login-history/post/{id}")
+def post_login_history(id,session_id,login_time,login_duration):
+    cursor = cnx.cursor()
+    cursor.execute(f"INSERT INTO LoginHistory (student_id, session_id, login_time, login_duration) VALUES ({id}, {session_id}, {login_time}, {login_duration});")
+    rows = [dict(zip(cursor.column_names, row)) for row in cursor]
+    cursor.close()
+    return {"status": "ok", "rows": rows}
+
+@app.get("/login-history/put/{session_id}/{new_login_duration}")
+def put_login_history(session_id,new_login_duration):
+    cursor = cnx.cursor()
+    cursor.execute(f"UPDATE `LoginHistory` SET `login_duration`={new_login_duration} WHERE session_id = {session_id};")
+    rows = [dict(zip(cursor.column_names, row)) for row in cursor]
+    cursor.close()
+    return {"status": "ok", "rows": rows}
+
+    
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", port=8000)
